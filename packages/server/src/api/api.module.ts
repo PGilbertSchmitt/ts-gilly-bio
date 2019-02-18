9f95c1419af3755c3b496903310ecf14fb9ee804
P. Gilbert Schmitt <pgilbertschmitt@gmail.com>
2019-02-18 15:48:52 -0800

    Remove bug api example

--- packages/server/src/api/api.module.ts
+++ packages/server/src/api/api.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
 
-import { BugsModule } from 'api/bugs/bugs.module';
 import { ProjectsModule } from 'api/projects/projects.module';
 
 @Module({
   imports: [
-    BugsModule,
     ProjectsModule,
   ],
 })
