52d8800fbbf214bb95e369151d459eab01a4d741
P. Gilbert Schmitt <pgilbertschmitt@gmail.com>
2019-02-18 15:41:28 -0800

    Add database provider

---
+++ packages/server/src/database/db.provider.ts
@@ -0,0 +1,20 @@
+import mongoose, { connect } from 'mongoose';
+
+import config from '@root/db.config';
+
+export const dbToken = 'DB_CONNECTION_TOKEN';
+export const dbProviders = [
+  {
+    provide: dbToken,
+    useFactory: async (): Promise<typeof mongoose> => (
+      await connect(`mongodb://localhost/${config.database}`, {
+        user: config.username,
+        pass: config.password,
+
+        // Prevents Mongo client from using deprecated methods
+        useNewUrlParser: true,
+        useCreateIndex: true,
+      })
+    ),
+  },
+];
