eda3598e63ff3f54e54302b9fd57e22721043f48
P. Gilbert Schmitt <pgilbertschmitt@gmail.com>
2019-03-23 13:25:31 -0700

    Use public folder to serve non-api assets

--- packages/server/src/page/page.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class PageService {
-  getHello(): string {
-    return 'Hello World!';
-  }
-}
--- packages/server/src/root.module.ts
+++ packages/server/src/root.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
 
-import { PageModule } from './page/page.module';
 import { APIModule } from './api/api.module';
 
 @Module({
   imports: [
-    PageModule,
     APIModule,
   ],
 })
