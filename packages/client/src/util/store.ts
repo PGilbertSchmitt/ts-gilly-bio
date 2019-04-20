81cb237b65d0d8922d3b60e9bb046bafb57fa88f
P. Gilbert Schmitt <pgilbertschmitt@gmail.com>
2019-04-20 13:05:07 -0700

    Connect redux devtools extension

--- packages/client/src/util/store.ts
+++ packages/client/src/util/store.ts
@@ -1,5 +1,6 @@
 import { createStore, applyMiddleware, Action } from 'redux';
 import createSagaMiddleware from 'redux-saga';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import RootReducer, { RootState } from '@reducers/_root_reducer';
 import RootSaga from '@sagas/_root_saga';
@@ -8,6 +9,8 @@ const sagaMiddleware = createSagaMiddleware();
 
 export default createStore<RootState, Action, {}, {}>(
   RootReducer,
-  applyMiddleware(sagaMiddleware),
+  composeWithDevTools(
+    applyMiddleware(sagaMiddleware),
+  ),
 );
 sagaMiddleware.run(RootSaga);
