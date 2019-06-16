e0eaf91d197fbd8f75e3113f7bee16fef3a6c8b7
PGilbertSchmitt <pgilbertschmitt@gmail.com>
2019-06-16 12:18:40 -0700

    Create refresh-based render loop

--- packages/client/src/components/core/main.tsx
+++ packages/client/src/components/core/main.tsx
@@ -1,12 +1,13 @@
 import React, { FunctionComponent as FC } from 'react';
 import { Route } from 'react-router-dom';
 
+import { AppProps } from '@comp/core/root';
 import PageBase from '@comp/core/page_base';
 import HomePage from '@comp/content/home_page';
 import BlogPage from '@comp/content/blog_page';
 import ProjectPage from '@comp/content/projects/project_page';
 
-const Main: FC = () => (
+const Main: FC<AppProps> = (props) => (
   <PageBase>
     <Route path='/' component={HomePage} exact />
     <Route path='/projects' component={ProjectPage} />
--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -1,11 +1,37 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 
 import Root from '@comp/core/root';
 
-document.addEventListener('DOMContentLoaded', () => {
-  ReactDOM.render(
-    <Root />,
-    document.getElementById('root'),
-  );
-});
+const hooks = {};
+const store = {
+  val: 5,
+  foo: 'bar',
+};
+
+const renderState = {
+  frame: 0,
+  dirty: true,
+  invalidate: () => renderState.dirty = true,
+};
+
+const paint = () => {
+  if (renderState.dirty) {
+    console.log(`Painting frame ${renderState.frame}`);
+    render(
+      <Root
+        hooks={hooks}
+        store={store}
+        refresh={renderState.invalidate} />,
+      document.getElementById('root'),
+    );
+    renderState.dirty = false;
+  }
+};
+
+const nextScreenTick = () => {
+  paint();
+  window.requestAnimationFrame(nextScreenTick);
+};
+
+window.onload = nextScreenTick;
