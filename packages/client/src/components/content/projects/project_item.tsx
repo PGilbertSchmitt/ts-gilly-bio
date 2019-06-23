40dde3eed27d3e8e587fc19d8a7dfe23a56f14b4
PGilbertSchmitt <pgilbertschmitt@gmail.com>
2019-06-23 12:36:47 -0700

    Update projects to remove redux dep

--- packages/client/src/components/content/projects/project_item.tsx
+++ packages/client/src/components/content/projects/project_item.tsx
@@ -1,78 +1,22 @@
 import React, { FunctionComponent as FC, useEffect } from 'react';
-import { Dispatch } from 'redux';
-import { connect } from 'react-redux';
-import { RouteComponentProps } from 'react-router-dom';
-import has from 'ramda/es/has';
+import { StateProjectItem } from '@gilly/common';
 
 import Marked from '@comp/content/marked/marked';
-import { StateProjectItem } from '@gilly/common';
-import { fetchProject } from '@actions/project_actions';
-import { RootState } from '@reducers/_root_reducer';
 
-type StateProps = {
-  loading: true;
-} | {
-  loading: false;
-  project: StateProjectItem;
-};
-
-interface DispatchProps {
-  loadProject: (slug: string) => void;
+interface ProjectItemProps {
+  project?: StateProjectItem;
 }
 
-type RouteProps = RouteComponentProps<{ slug: string }>;
-
-type Props = StateProps & DispatchProps & RouteProps;
-
-const ProjectItem: FC<Props> = (props) => {
-  const { loading, match, loadProject } = props;
-  useEffect(() => {
-    if (loading) {
-      loadProject(match.params.slug);
-    }
-  }, [loading]);
-
-  if (props.loading) {
+const ProjectItem: FC<ProjectItemProps> = ({ project }) => {
+  if (project) {
     return (
-      <div>Loading...</div>
-    );
-  }
-
-  const { project } = props;
-
-  // Doesn't seem likely, but best to be prepared. Maybe this should be a 404.
-  if (project === undefined) {
-    return (
-      <div>Hmm, there doesn't seem to be a project at ${match.url}</div>
+      <Marked customClass='project-page' src={project.content} />
     );
   }
 
   return (
-    <Marked customClass='project-page' src={project.content} />
+    <div>Loading...</div>
   );
 };
 
-const mapStateToProps = ({ projects }: RootState, { match }: RouteProps): StateProps => {
-  const { slug } = match.params;
-  const loading = !has(slug, projects);
-
-  if (loading) {
-    return { loading };
-  }
-
-  return {
-    loading,
-    project: projects[slug]
-  };
-};
-
-const mapDispatchToProps = (dispatch: Dispatch): DispatchProps => ({
-  loadProject: (slug: string) => {
-    dispatch(fetchProject(slug));
-  },
-});
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps,
-)(ProjectItem);
+export default ProjectItem;
--- packages/client/src/components/content/projects/project_page.tsx
+++ packages/client/src/components/content/projects/project_page.tsx
@@ -1,13 +1,43 @@
 import React, { FunctionComponent as FC } from 'react';
-import { Route, RouteComponentProps } from 'react-router-dom';
+import { Route, match as matchType, Redirect } from 'react-router-dom';
 
 import ProjectIndex from './project_index';
 import ProjectItem from './project_item';
 
-const ProjectPage: FC<RouteComponentProps> = ({ match }) => (
+import { hooks, store } from '@src/store/root_state';
+
+interface ProjectProps {
+  match: matchType<{ slug: string; }>;
+  projectIndex: typeof store.projectStore.projectIndex;
+  projects: typeof store.projectStore.projects;
+  refresh: () => void;
+}
+
+const ProjectPage: FC<ProjectProps> = ({ match, projects, projectIndex, refresh }) => (
   <>
-    <Route exact path={`${match.path}`} component={ProjectIndex} />
-    <Route path={`${match.path}/:slug`} component={ProjectItem} />
+    <Route exact path={`${match.path}`} render={() => {
+      if (projectIndex.length === 0) {
+        hooks.fetchProjects().then(refresh);
+      }
+      return (
+        <ProjectIndex index={projectIndex} />
+      );
+    }} />
+    <Route path={`${match.path}/:slug`} render={({ match: { params } }) => {
+      const { slug } = params;
+      const project = projects[slug];
+      console.log(project);
+      if (project === null) {
+        return (<Redirect to='/projects' />);
+      }
+
+      if (project === undefined) {
+        hooks.fetchProject(slug).then(refresh);
+      }
+      return (
+        <ProjectItem project={project} />
+      );
+    }} />
   </>
 );
 
